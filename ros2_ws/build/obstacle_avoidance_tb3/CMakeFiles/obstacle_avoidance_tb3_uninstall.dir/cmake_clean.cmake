46a9f7e81f723b655ff80b7ac3d5d3d664c3ef0b
Md Jamal Uddin <md.uddin2@stud.fra-uas.de>
2026-03-12 17:35:14 +0100

    clean

--- ros2_ws/build/obstacle_avoidance_tb3/CMakeFiles/obstacle_avoidance_tb3_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/obstacle_avoidance_tb3_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/obstacle_avoidance_tb3_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- ros2_ws/build/obstacle_avoidance_tb3/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vboxuser/ros2_ws/src/obstacle_avoidance_tb3
-# Build directory: /home/vboxuser/ros2_ws/build/obstacle_avoidance_tb3
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
